d2b17e80df121640d29fb9d117d7877851b0f875
aaronlad <aaronkirkham07@gmail.com>
2018-10-04 00:40:01 +0100

    fix tests on node versions without import support

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,18 +1,17 @@
-import { set, get } from '../lib/utils'
+const utils = require('../lib/utils');
 
 describe('Utils', () => {
   describe('set', () => {
     it('should build the nested object properties', () => {
       const obj = {};
-      set(obj, ['x', 'y', 'z'], { key: 'value' });
+      utils.set(obj, ['x', 'y', 'z'], { key: 'value' });
       expect(obj.x.y.z.key).toEqual('value');
     })
   })
 
   describe('get', () => {
     it('should return an empty array', () => {
-      const result = get({}, ['a', 'b', 'c'], []);
-      expect(result).toHaveLength(0);
+      expect(utils.get({}, ['a', 'b', 'c'], [])).toHaveLength(0);
     })
   })
 })
